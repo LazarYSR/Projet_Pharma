e926cdb88e430d0fa8a77a49901874713de9442b
LazarYSR <official.lazaryasser@gmail.com>
2024-03-26 15:14:32 +0100

    first commit

---
+++ Projet_PharmaService/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_PharmaService.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Principal()
+        {
+            return View("Principal");
+        }
+    }
+}
