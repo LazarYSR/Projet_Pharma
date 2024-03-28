f53b162a15c51a779c972521406aa75edcadb61c
LazarYSR <official.lazaryasser@gmail.com>
2024-03-29 00:32:28 +0100

    Ajoute Des Trois Classes

---
+++ Projet_PharmaService/Models/Paiement.cs
@@ -0,0 +1,9 @@
+﻿namespace Projet_PharmaService.Models
+{
+    public class Paiement
+    {
+        public int PaiementId { get; set; }
+        public String PaiementMode { get; set; }
+        public ICollection<Facture> Factures { get; set; }
+    }
+}
