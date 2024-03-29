660d78cdd71f1eabbf95415a207b176d75e29653
MALKI Mohammed <malki.mohammed.1997@gmail.com>
2024-03-29 01:32:20 +0100

    add admin-panier-archivepanier

---
+++ Projet_PharmaService/Models/Panier.cs
@@ -0,0 +1,27 @@
+﻿namespace Projet_PharmaService.Models
+{
+    public class Panier
+    {
+
+        public int Id { get; set; }
+
+        public double prixTotal { get; set; }
+
+        public int codePrmo { get; set; }
+
+        public string satuts { get; set; }
+
+        public DateTime dateCreation { get; set; }
+
+        public Client client { get; set; }
+
+        public int IdClient { get; set; }
+
+        public Pharmacy pharmacy { get; set; }
+
+        public int IdPharmacy { get; set; }
+
+        public ICollection<Medicament> medicaments { get; set; }
+
+    }
+}
